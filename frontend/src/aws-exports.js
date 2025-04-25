a9f83ccfd39e62327c1621ce94c746aef42d2cf6
yasaswinijamma <yash19495@gmail.com>
2025-04-25 17:00:09 -0500

    Add aws-exports.js to frontend/src

---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,15 @@
+const awsmobile = {
+    aws_project_region: 'us-east-2',
+    aws_appsync_graphqlEndpoint: '',
+    aws_appsync_region: 'us-east-2',
+    aws_appsync_authenticationType: 'API_KEY',
+    aws_appsync_apiKey: '',
+    aws_cloud_logic_custom: [
+        {
+            name: 'RealEstateAPI',
+            endpoint: 'https://placeholder.execute-api.us-east-2.amazonaws.com/prod',  // Placeholder; will update after API Gateway setup
+            region: 'us-east-2'
+        }
+    ]
+};
+export default awsmobile;
