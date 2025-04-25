e80a965abc469e92cd80f0f5915cf23f4e3f0ec9
yasaswinijamma <yash19495@gmail.com>
2025-04-25 17:42:11 -0500

    Add Amplify configuration to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-  import ReactDOM from 'react-dom/client';
-  import App from './App';
-  import './App.css';
+import ReactDOM from 'react-dom/client';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+import App from './App';
+import './App.css';
 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+Amplify.configure(awsExports);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+   );
