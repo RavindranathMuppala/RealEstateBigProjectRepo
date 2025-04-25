a0a52d99d9fe52a4755c8fac61f8e62ef5759f26
yasaswinijamma <yash19495@gmail.com>
2025-04-25 17:47:12 -0500

    Fix aws-amplify import in index.js to use named export

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Amplify from 'aws-amplify';
+import {Amplify} from 'aws-amplify';
 import awsExports from './aws-exports';
 import App from './App';
 import './App.css';
@@ -10,6 +10,6 @@ Amplify.configure(awsExports);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <App />
+    <App />
   </React.StrictMode>
    );
